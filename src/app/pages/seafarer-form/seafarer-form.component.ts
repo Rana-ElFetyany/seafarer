066ba22f828287d824c71c5a94e972b1bdeedf6e
Rana-ElFetyany <ranafet777@gmail.com>
2025-09-23 17:15:07 +0300

    initial the project

---
+++ src/app/pages/seafarer-form/seafarer-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seafarer-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './seafarer-form.component.html',
+  styleUrl: './seafarer-form.component.scss'
+})
+export class SeafarerFormComponent {
+
+}
---
+++ src/app/pages/seafarers-list/seafarers-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seafarers-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './seafarers-list.component.html',
+  styleUrl: './seafarers-list.component.scss'
+})
+export class SeafarersListComponent {
+
+}
